eccebfb45a4b2583bd080b70ec5f8ea21bac6f86
Archana SathiyaSeelan <77286741+ArchanaSathiyaseelans@users.noreply.github.com>
2026-09-24 00:33:05 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
-  withCredentials: true,   // 🔑 ensures cookies are sent
-});
-
-
+export const api=axios.create({
+    baseURL:import.meta.env.VITE_SERVER_URL,
+    withCredentials:true
+})
